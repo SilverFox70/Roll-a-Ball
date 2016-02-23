fc7c1eea385b83963b89d46a8ec42be7966b7cd1
SilverFox70 <wfbrinkert@gmail.com>
2016-02-22 19:55:50 -0800

    Tutorial game for Unity
    This is tutorial for a game I completed in Unity using C#

---
+++ Assets/_Scene/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text CountText;
+	public Text WinText;
+	private int count;
+
+	void start ()
+	{
+		count = 0;
+		CountText = GetComponent<Text> ();
+		WinText = GetComponent<Text> ();
+		WinText.text = "";
+		SetCountText ();
+
+	}
+
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	 void SetCountText(){
+		CountText.text = "Count: " + count.ToString();
+		if (count >= 12) {
+			WinText.text = "YOU WIN!";
+		}
+
+	}
+}
